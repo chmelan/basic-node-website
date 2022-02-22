9ea1542831d856e6bfd18c010ca4867e4a96a951
Nick Chmela <nickchmela@gmail.com>
2022-02-22 15:08:46 -0500

    completed project

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs').promises
+
+const hostname = 'localhost';
+const port = 8080;
+
+const requestListener = function (req, res) {
+   switch (req.url){
+       case "/":
+           fs.readFile(__dirname + "/index.html").then(contents =>{
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+           })
+        break;
+        case "/about":
+           fs.readFile(__dirname + "/about.html").then(contents =>{
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+           })
+        break;
+        case "/contact-me":
+           fs.readFile(__dirname + "/contact-me.html").then(contents =>{
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+           })
+        break;
+        default:
+            fs.readFile(__dirname + "/404.html").then(contents =>{
+                res.setHeader("Content-Type", "text/html");
+                res.writeHead(404);
+                res.end(contents);
+               })
+   }
+};
+  
+const server = http.createServer(requestListener);
+
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
